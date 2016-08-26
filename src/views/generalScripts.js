16b5432d24e87b69711b698f15d0aa0ac0220038
Guilherme Vieira <super.driver.512@gmail.com>
2016-08-26 08:28:33 -0300

    WIP.

--- src/views/generalScripts.js
+++
@@ -1,9 +0,0 @@
-const $ = require('jquery');
-
-exports.clientScripts = {
-  dataHrefButtonClick: () =>
-    $('body').on('click', 'button[data-href]', ev => {
-      ev.preventDefault();
-      location.href = $(ev.target).attr('data-href');
-    }),
-};
