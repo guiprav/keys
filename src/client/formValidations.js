2551a3628b5017a342beb8f9820a2f4d7a73499b
Guilherme Vieira <super.driver.512@gmail.com>
2016-08-29 15:36:35 -0300

    [Admin] Allow blank non-required e-mail fields.

--- src/client/formValidations.js
+++ src/client/formValidations.js
@@ -20,10 +20,11 @@ $(() => {
 
     $form.find('[type="email"]').each((i, el) => {
       const $el = $(el);
+      const val = $el.val();
 
       const re = /^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9_\-\.]+\.[a-zA-Z0-9_\-]+$/;
 
-      if (!re.test($el.val())) {
+      if (val && !re.test(val)) {
         ev.preventDefault();
         Keys.flagFieldError($el, 'E-mail inválido.');
       }
