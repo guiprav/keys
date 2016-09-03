b218cf1d028fd227c947b0b39aeb0b113b3ba134
Guilherme Vieira <super.driver.512@gmail.com>
2016-09-03 17:03:05 -0300

    Make views.dateTime support falsy timestamps.

--- src/views/dateTime.js
+++ src/views/dateTime.js
@@ -1,3 +1,9 @@
 const moment = require('moment');
 
-module.exports = (req, ts, fmt = 'lll') => moment(ts).format(fmt);
+module.exports = (req, ts, fmt = 'lll') => {
+  if (!ts) {
+    return 'N/A';
+  }
+
+  return moment(ts).format(fmt);
+};
