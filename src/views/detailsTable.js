40869b2ba6363453da19a777419995a1ffa1171a
Guilherme Vieira <super.driver.512@gmail.com>
2016-09-12 15:56:05 -0300

    Support hidden details table rows.

--- src/views/detailsTable.js
+++ src/views/detailsTable.js
@@ -8,6 +8,7 @@ module.exports = async (
   fieldSet = req.action.views.fieldSet(req, record),
 ) => {
   const fieldRows = await Qh.deepWhen(fieldSet.map(field => ({
+    hidden: field.hidden,
     label: field.label && field.label(req, record),
     data: field.data(req, record),
   })));
@@ -19,9 +20,17 @@ module.exports = async (
       ),
     ),
 
-    fieldRows.map(row => tr('.keysDetailsTable_dataRow',
-      th('.keysDetailsTable_dataHeader', row.label),
-      td('.keysDetailsTable_dataCell', row.data),
-    )),
+    fieldRows.map(row => {
+      const tRow = tr('.keysDetailsTable_dataRow',
+        th('.keysDetailsTable_dataHeader', row.label),
+        td('.keysDetailsTable_dataCell', row.data),
+      );
+
+      if (row.hidden) {
+        tRow.classList.add('keysHidden');
+      }
+
+      return tRow;
+    }),
   );
 };
